210cb8743c1f76ae4544e36531490b14b18deefd
Cornholio <darkokrstev@gmail.com>
2024-07-07 22:03:11 +0200

    Add brands container div

--- wp-content/plugins/lifeline/src/Lifeline/Frontend/views/lifeline-manufacturers.php
+++ wp-content/plugins/lifeline/src/Lifeline/Frontend/views/lifeline-manufacturers.php
@@ -9,10 +9,11 @@
         $term = get_term_by( 'slug', $slug, 'pa_manufacturer' );
 
         echo "<h2>$term->name</h2>";
-
-
-        echo do_shortcode('[product_attribute attribute="manufacturer" terms="' . $slug . '" operator="IN" paginate="true"]');
-        // echo do_shortcode('[products terms="' . $slug . '" terms_operator="IN" paginate="true"]');
+?>
+        <div class="brands-container">
+            <?php echo do_shortcode('[product_attribute attribute="manufacturer" terms="' . $slug . '" operator="IN" paginate="true"]'); ?>
+        </div>
+<?php
     } else {
         foreach ($terms as $term) { ?>
             <a href="<?php echo home_url('/brands/' . $term->slug . '/'); ?>">
