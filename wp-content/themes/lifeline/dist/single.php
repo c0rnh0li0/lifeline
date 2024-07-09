f29606f5a2da4201d36f9fbf2ad64c637ffd6e56
Cornholio <darkokrstev@gmail.com>
2024-07-09 21:57:06 +0200

    Fix products columns and limit

--- wp-content/themes/lifeline/dist/single.php
+++ wp-content/themes/lifeline/dist/single.php
@@ -1,6 +1,8 @@
 <?php
 if ( in_category('product') ) {
 	include 'single-product.php';
+} else if ( in_category('blog') ) {
+	include 'single-blog.php';	
 } else {
 	include 'single-default.php';}
 ?>
