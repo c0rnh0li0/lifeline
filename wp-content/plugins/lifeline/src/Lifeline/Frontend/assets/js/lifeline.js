a1cfb4a5cd28f57a301d4c15af3ab33eec764b02
kmatoski <kmatoski@gmail.com>
2024-09-04 21:50:51 +0200

    Css popravki 2

--- wp-content/plugins/lifeline/src/Lifeline/Frontend/assets/js/lifeline.js
+++ wp-content/plugins/lifeline/src/Lifeline/Frontend/assets/js/lifeline.js
@@ -94,6 +94,26 @@ window.Lifeline || (window.Lifeline = {});
                 infinite: true,
                 autoplay: false,
                 // autoplaySpeed: 2000,
+                responsive: [
+                    {
+                      breakpoint: 1200,
+                      settings: {
+                        slidesToShow: 4,
+                      }
+                    },
+                    {
+                      breakpoint: 900,
+                      settings: {
+                        slidesToShow: 3,
+                      }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                          slidesToShow: 2,
+                        }
+                      }
+                  ]
             };
 
             $('.lifeline-slider-group .products').slick(slick_opts);
