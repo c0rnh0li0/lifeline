8b6901179b5f16cf926c2c367efb5619e33f47cb
kmatoski <kmatoski@gmail.com>
2024-11-20 12:05:41 +0100

    Brendovite so slika 3

--- wp-content/themes/lifeline/loop-templates/content-homepage.php
+++ wp-content/themes/lifeline/loop-templates/content-homepage.php
@@ -190,7 +190,7 @@ defined( 'ABSPATH' ) || exit;
 								$brands = array_rand($terms, $total_brands);
 
 								foreach ($brands as $brand) { ?> 
-									<a href="<?php echo $single_brand_url; ?>" class="<?php echo $terms[$brand]->slug; ?>">
+									<a href="/brands/?manufacturer=<?php echo $terms[$brand]->slug; ?>" class="<?php echo $terms[$brand]->slug; ?>">
 										<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/brands/<?php echo $terms[$brand]->slug; ?>.webp" alt="<?php echo $terms[$brand]->slug; ?>" />
 									</a>
 							<?php } } ?>							
